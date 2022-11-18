dd9be0c9c0930009d064dbe6c9a8bf3b8b3ca11a
FR_Jess <fr_jess@icloud.com>
2022-11-18 16:03:05 +0100

    [fix] search position

--- src/components/AppSearch.vue
+++ src/components/AppSearch.vue
@@ -13,15 +13,16 @@ export default {
 </script>
 
 <template>
-  <div class="container">
-    <div class="col-2 py-3">
-      <select @change="$emit('startSearch')" v-model="store.categoryToSearch" class="form-select">
-        <option value="" selected>Select category</option>
-        <option value="Breaking Bad">Breaking Bad</option>
-        <option value="Better Call Saul">Better Call Saul</option>
-      </select>
-    </div>
+  <div class="jt-container">
+    <select @change="$emit('startSearch')" v-model="store.categoryToSearch" class="form-select">
+      <option value="" selected>Select category</option>
+      <option value="Breaking Bad">Breaking Bad</option>
+      <option value="Better Call Saul">Better Call Saul</option>
+    </select>
+
   </div>
+
+
   
 </template>
 
@@ -29,10 +30,15 @@ export default {
 @use "../styles/partials/variables" as *;
 @use "../styles/partials/mixins" as *;
 
-// p{
-//   font-family: $main-font;
-//   color: $-color;
-//   background-color: white;
-// }
+.jt-container{
+  width: 70%;
+  margin: 0 auto;
+  select{
+    width: 200px;
+    margin-bottom: 20px;
+  }
+
+}
+
 
 </style>
